9ba68b8e92b16d8f5439976bf11d43735de6402e
zhangliyuan01 <zhangliyuan01@baidu.com>
2016-07-22 13:09:21 +0800

    scala
    init hello world

---
+++ src/com/doggie/HelloWorld.scala
@@ -0,0 +1,13 @@
+package com.doggie
+
+import java.util
+import java.util.HashMap;
+/**
+  * Created by baidu on 16/7/22.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+//    HashMap hm = new util.HashMap[val,val]()
+  }
+}
